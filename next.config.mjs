b48df735bf2cdb4c5e80386a756085e1a83472a8
Lucky <luckysolanki902@gmail.com>
2025-02-04 03:00:06 +0530

    hotfix: diabling eslint in production and deleted pixel-xml provider

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'd26w01jhwuuxpo.cloudfront.net',
-          },
-          {
-            protocol: 'https',
-            hostname: 'd3efr8i3xj2spn.cloudfront.net',
-          },
-          {
-            protocol: 'https',
-            hostname: 'maddy-assets.s3.ap-south-1.amazonaws.com',
-          }
-        ],
+  eslint: {
+    ignoreDuringBuilds: true, // Disables ESLint during production builds
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'd26w01jhwuuxpo.cloudfront.net',
       },
-
+      {
+        protocol: 'https',
+        hostname: 'd3efr8i3xj2spn.cloudfront.net',
+      },
+      {
+        protocol: 'https',
+        hostname: 'maddy-assets.s3.ap-south-1.amazonaws.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;

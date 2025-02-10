a8305ca16b988e4bef244a0bf554213e4b26bb9e
Lucky <luckysolanki902@gmail.com>
2025-02-10 22:00:25 +0530

    feat: catalogue pdf

--- src/app/admin/departments/production/page.js
+++ src/app/admin/departments/production/page.js
@@ -4,6 +4,7 @@ import React from 'react';
 export default function Production() {
   const optionsWithLinks = [
     { text: 'Download Templates', link: '/admin/download/download-production-templates' },
+    { text: 'Download Catalogue', link: '/admin/download/download-catalogue' },
     { text: 'Orders Dashboard', link: '/admin/manage/orders/order-list' },
     { text: 'Design Search', link: '/admin/manage/orders/sku-search' },
     { text: 'Analytics Dashboard', link: '/admin/analytics' },

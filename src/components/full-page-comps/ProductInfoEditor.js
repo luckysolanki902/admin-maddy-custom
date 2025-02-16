dc7bed084fb0d6d18abe4de99f8db97b52c35491
Harsh Pathak <jkpathak83195@gmail.com>
2025-02-16 18:09:25 +0530

    hotfix: remove customParagraph class temporarily

--- src/components/full-page-comps/ProductInfoEditor.js
+++ src/components/full-page-comps/ProductInfoEditor.js
@@ -284,7 +284,7 @@ const ProductInfoAdminEditor = () => {
             inlineToolbar: true,
           },
           paragraph: {
-            class: CustomParagraph,
+            class: Paragraph,
             inlineToolbar: ["bold", "italic", "link"],
           },
           inlineCode: {

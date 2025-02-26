273269dcbbe7f9def29623c4cfec6ccdbdadc4e2
web_dev <sukritprakash2020@gmail,com>
2025-02-26 14:04:14 +0530

    feat:  created options schema  and changes in specific category schema

---
+++ src/models/Option.js
@@ -0,0 +1,41 @@
+// /models/OptionSchema.js
+const mongoose = require('mongoose');
+
+const OptionSchema = new mongoose.Schema({
+  product: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Product',
+    required: true,
+  },
+  //sku
+  sku: {
+    type: String,
+    required: true,
+    unique: true,   
+  },
+  // A flexible approach to store option details using a Map
+  optionDetails: {
+    // Example: { color: 'red', size: 'M' }
+      // Example: { color: 'blue'}
+    type: Map,
+    of: String,
+    required: false,
+  },
+
+
+  availableQuantity: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  reservedQuantity: {
+    type: Number,
+    default: 0,
+  },
+  reorderLevel: {
+    type: Number,
+    default: 50,
+  },
+}, { timestamps: true });
+
+module.exports = mongoose.model('Option',OptionSchema);
